24d9aa91d911c7c5f4461be7e50b1b096072e0aa
Li He <jet.in.brain@gmail.com>
2014-07-04 15:48:49 +0800

    add student index page

--- jw_djtu/settings.py
+++ jw_djtu/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'students',
+    'teachers'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -68,7 +69,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'China/Shanghai'
 
 USE_I18N = True
 
@@ -79,6 +80,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATIC_URL = '/static/'
 
--- students/views.py
+++ students/views.py
@@ -1,3 +1,7 @@
+from django.shortcuts import render_to_response
 from django.shortcuts import render
 
+
 # Create your views here.
+def student_index(request):
+    return render_to_response('index.html')
